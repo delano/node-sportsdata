0ba07196b04991ffae7a38692c8d1ebe5c4de0de
Delano Mandelbaum <delano@jbfkids.com>
2015-01-19 15:33:51 -0800

    Cleaned up NHL example

--- example/nhl.js
+++ example/nhl.js
@@ -1,17 +1,24 @@
-/** SPORTSDATA API - NHL Example
+/**
+* SPORTSDATA API - NHL Example
 *
 * Usage:
 *        $ export SPORTSDATA_NHL=[APIKEY]
 *        $ node example/nhl.js
+*
+* References:
+*   http://developer.sportsdatallc.com/io-docs
+*
 **/
 
 var sportsdata = require('../index').NHL;
 var APIKEY = process.env.SPORTSDATA_NHL;
+var APIVERSION = 3;       // NHL API is currently v3 as of 2015-01-19
+var SEASONID = 2014;      // Season is the year the season started
+var SEASONPART = 'REG';   // One of: REG (regular season), PST (post-season)
 
 console.log('Using key:', APIKEY);
 
-// Init the object with the access level, version, apikey, year, and season you care about
-sportsdata.init('t', 3, APIKEY, '2014', 'REG');
+sportsdata.init('t', APIVERSION, APIKEY, SEASONID, SEASONPART);
 
 sportsdata.getSeasonSchedule(function(error, schedule) {
   if (error) {
